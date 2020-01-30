4df159fc7e604b92b7afe32a7aeb3b97be3054c8
Emilian Cole <emiliancole@gmail.com>
2020-01-30 12:01:26 +0100

    Update listFlashSvgFunctions.06.php

--- v06/listFlashSvgFunctions.06.php
+++ v06/listFlashSvgFunctions.06.php
@@ -1,1 +1,17 @@
+<?php
+// LIST ALL DEFINED FUNCTIONS IN A PHP FILE
+
+$lines = file('flashSvg.inc.06.php');
+$i=0;
+
+foreach ($lines as $line_num => $line) {
+$fline=explode(" ",$line);
+if ($fline[0]=="//") {
+    echo "$i-Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
+    $i++;  }
+}
+
+echo "<hr>";
+
+?>
 
